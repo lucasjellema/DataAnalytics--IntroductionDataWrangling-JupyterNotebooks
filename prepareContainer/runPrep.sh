de10ef41d7621c8d6f74512ca9bf7ac9cd760a12
Lucas Jellema <lucas.jellema@amis.nl>
2019-02-06 14:37:34 +0100

    Removing ls command

--- prepareContainer/runPrep.sh
+++ prepareContainer/runPrep.sh
@@ -1,9 +1,9 @@
 # copy the script into the container; note: the resulting file is owned by root and it not executable
 docker cp prepareContainer.sh jupyter:/tmp/prepareContainerRoot.sh
 # copy the script to make it accessible and executable 
-docker exec -it jupyter bash -c 'cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh &&ls'
+docker exec -it jupyter bash -c 'cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
 # execute the script inside the container - to install a number of packages
-docker exec -it jupyter bash -c 'ls -l && ./prepareContainer.sh'
+docker exec -it jupyter bash -c './prepareContainer.sh'
 # # restart the docker container
 docker restart jupyter
 # perhaps clone a GitHub repo inside the Jupyter container
