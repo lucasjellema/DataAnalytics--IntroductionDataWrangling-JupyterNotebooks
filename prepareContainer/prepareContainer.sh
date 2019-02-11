e172abad0a765187eccb4388bda86e6a2206d442
Lucas Jellema <lucas.jellema@amis.nl>
2019-02-11 17:36:33 +0100

    cloning the workshop repo into the Docker container

--- prepareContainer/prepareContainer.sh
+++ prepareContainer/prepareContainer.sh
@@ -10,26 +10,23 @@ pip install -U nltk
 # apparently the base image already contain ipywidgets
 #pip install ipywidgets
 pip install pyarrow
-# word_cloud has a fix dependency on an older version of pandas - and when installed will overwrite the current one that comes in the Docker image
+# word_cloud has a fixed dependency on an older version of pandas - and when installed will overwrite the current one that comes in the Docker image
 # here I am force installing the latest pandas - required by the widgets notebook; word_cloud will still work
 pip install --upgrade pandas
-# perhaps clone a github repo into /home/jovyan/work
 cd ~/work
 git clone http://bitbucket.org/hrojas/learn-pandas
+# adding notebook to play with ipywidgets
 git clone https://github.com/WillKoehrsen/Data-Analysis
 cp -R Data-Analysis/widgets ~/work
-# next follows a round about way to get a single file into the running container
 mkdir temp
 cd temp
+# next follows a round about way to get a single file into the running container
 git clone https://github.com/lucasjellema/katacoda-scenarios
 cp katacoda-scenarios/jupyter-notebook/assets/introPythonForDataAnalysis.ipynb ~/work
-# adding notebook to play with ipywidgets
+# clone the data wrangling workshop github repo
+git clone https://github.com/lucasjellema/DataAnalytics--IntroductionDataWrangling-JupyterNotebooks
+# then move folder CaseOfOracleOpenWorld2018 under ~/work
+mv DataAnalytics--IntroductionDataWrangling-JupyterNotebooks/CaseOfOracleOpenWorld2018 ~/work
+
 cd ~/work
 rm -Rf temp 
-
-
-For example: open 
-
-and run pythonForDataAnalysis.ipynb in the work folder. Or open and run Example_word_clouds.ipynb. Or open folder learn-pandas/lessons and start with Python_101.ipynb or Cookbook - Select.ipynb. 
-
-The folder work/Data-Analysis contains many notebooks created by Will Koehrsen, who writes many great articles about Data Science and uses Jupyter Notebooks frequently (see his GitHub Repo: https://github.com/WillKoehrsen/Data-Analysis ). 
